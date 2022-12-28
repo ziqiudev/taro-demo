566c86f61e33e21be0ce390dcac9bede868d6a69
ziqiudev <qqqiuwk@gmail.com>
2022-12-28 11:55:05 +0800

    fix: taro回退到3.5.4, 3.5.10的tabbar有问题

--- config/index.js
+++ config/index.js
@@ -16,12 +16,13 @@ const config = {
     options: {},
   },
   framework: "react",
-  compiler: {
-    type: "webpack5",
-    prebundle: {
-      enable: false,
-    },
-  },
+  // compiler: {
+  //   type: "webpack5",
+  //   prebundle: {
+  //     enable: false,
+  //   },
+  // },
+  compiler: "webpack5",
   cache: {
     enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
